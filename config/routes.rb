eff65d4bf79b2bb189f6b987176d9566eaa2860a
Lina Torres <linatorres@MacBook-Pro-de-user.local>
2015-07-07 21:47:46 -0500

    Session Controller and Sign in Frontend

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'site#index'
+   get 'sign_in' => 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
