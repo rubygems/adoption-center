9ed30a9c042b4ecc0c9cc9f6d97cba34c188f2c6
Lina Torres <torresdiazlina@gmail.com>
2015-08-11 00:26:31 -0500

    search gems

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,8 @@
 class SiteController < ApplicationController
   def index
   end
+
+  def search_gems
+  	@gems = GemsAdoption.search(params[:search])
+  end
 end
--- app/models/gems_adoption.rb
+++ app/models/gems_adoption.rb
@@ -2,4 +2,9 @@ class GemsAdoption < ActiveRecord::Base
   belongs_to :user
   has_many :adoption_requests
   validates :gem, :user_id, presence: true
+
+  def self.search(search)
+	  where("gem like ?", "%#{search}%") 
+	end
+
 end
