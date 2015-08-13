d2e1bdb632423b6f73515b9867962158f29c2061
Lina Torres <torresdiazlina@gmail.com>
2015-08-13 00:32:44 -0500

    add partial and tests

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class SiteControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+   
+  test "should get index" do
+    get :index
+
+    assert_template :index
+    assert_select "title", "Home | RubyGems AdoptionCenter"
+    assert_response :success
+  end
+
+  test "should get search" do
+    get :search_gems, search: 'rails'
+
+    assert_template :search_gems
+    assert_select "title", "| RubyGems AdoptionCenter"
+    assert_response :success
+  end
+
 end
