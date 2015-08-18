31d7ec0225a437336399d124489df4a11036bd0b
Lina Torres <torresdiazlina@gmail.com>
2015-08-17 23:51:48 -0500

    Fix test

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -2,20 +2,13 @@ require 'test_helper'
 
 class SiteControllerTest < ActionController::TestCase
   
-   
-  test "should get index" do
-    get :index
-
-    assert_template :index
-    assert_select "title", "Home | RubyGems AdoptionCenter"
-    assert_response :success
-  end
 
   test "should get search" do
-    get :search_gems, search: 'rails'
+    get :search_gems, search: 'pg'
 
     assert_template :search_gems
     assert_select "title", "| RubyGems AdoptionCenter"
+    assert_select "#gem_name", "pg"
     assert_response :success
   end
 
