6571790049edcb5b016e6d0ff9e4201b1e0b7e96
Lina Torres <torresdiazlina@gmail.com>
2015-09-01 21:15:06 -0500

    WIP

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,9 @@
 class NotificationMailer < ApplicationMailer
 
-	def email_new_adoption_request(gem_owner , user, gem_for_adoption)
-		@gem_owner = gem_owner
+	def email_new_adoption_request( user, gem_for_adoption)
+		@gem_owner = gem_for_adoption.user
 		@user = user
 		@gem = gem_for_adoption
-		mail(to: @user.email, subject: 'RubyGems Adoption Center - New adoption request')
+		mail(to: @gem_owner.email, subject: 'RubyGems Adoption Center - New adoption request')
 	end
 end
