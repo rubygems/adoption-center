95110a6bafeea08446e8778797e94933216ec613
Lina Torres <torresdiazlina@gmail.com>
2015-09-10 00:20:13 -0500

    test

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -5,7 +5,7 @@ class GemsController < ApplicationController
 
   def show
     @ruby_gem = RubyGem.find(params[:id])
-    @gem_adoption = GemsAdoption.find_by(ruby_gem_id: params[:id])
+    @gem_adoption = GemsAdoption.find_by(id: params[:id])
     if @gem_adoption
       @adoption_request =  current_user.adoption_requests.find_by(gems_adoption_id: @gem_adoption.id)
     end
--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class NotificationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "new adoption request " do
+    email = NotificationMailer.email_new_adoption_request(users(:alfonso), gems_adoptions(:pg), adoption_requests(:one)).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
+
+    assert_equal ['me@example.com'], email.from
+    assert_equal ['ammancilla@gmail.com'], email.to
+    assert_equal 'RubyGems Adoption Center - New adoption request', email.subject
+ 
+	end
 end
