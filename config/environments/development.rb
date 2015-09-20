469e91550be79b06bfe830f7ac6653b86a5796d6
angela4713 <ang3l_gu@hotmail.com>
2015-09-20 13:23:15 -0500

    Emails for new adoption requests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  #letter opener gem
+  config.action_mailer.delivery_method = :letter_opener
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
