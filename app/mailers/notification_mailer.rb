5d97bae1ace9c1ee6c5d76eba209b5a5f7763ffa
angela4713 <ang3l_gu@hotmail.com>
2015-09-21 10:51:36 -0500

    Email Adoption request status

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,4 +6,19 @@ class NotificationMailer < ApplicationMailer
     @gem = gem_for_adoption
     mail(to: @gem_owner.email, from: 'me@example.com', subject: 'RubyGems Adoption Center - New adoption request')
   end
+
+  def email_adoption_request_accepted(user, gem_for_adoption)
+    @user = User.find(user)
+    @gem = gem_for_adoption
+    mail(to: @user.email, from: 'me@example.com', subject: 'RubyGems Adoption Center - Adoption Request Accepted')
+  end
+
+  def email_adoption_request_rejected(user, gem_adoption)
+    @user = User.find(user)
+    @gem_adoption = gem_adoption
+    users_rejected = AdoptionRequest.where(gems_adoption_id: @gem_adoption).where.not(user_id: @user)
+    users_rejected.each do |user_rejected|
+      mail(to: user_rejected.user.email, from: 'me@example.com', subject: 'RubyGems Adoption Center - Adoption Request Rejected')
+    end
+  end
 end
