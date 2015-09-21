342d8ccfb05767ad727e32dea143491b4c1f2ab8
angela4713 <ang3l_gu@hotmail.com>
2015-09-21 10:51:36 -0500

    Email Adoption request status

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -9,4 +9,20 @@ class NotificationMailerTest < ActionMailer::TestCase
     assert_equal ['angela4713@gmail.com'], email.to
     assert_equal 'RubyGems Adoption Center - New adoption request', email.subject
   end
+
+  test "adoption request accepted" do
+    email = NotificationMailer.email_adoption_request_accepted(users(:alfonso), ruby_gems(:pg)).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
+
+    assert_equal ['ammancilla@gmail.com'], email.to
+    assert_equal 'RubyGems Adoption Center - Adoption Request Accepted', email.subject
+  end
+
+  test "adoption request rejected" do
+    email = NotificationMailer.email_adoption_request_rejected(users(:alfonso), gems_adoptions(:pg)).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
+
+    assert_equal ['sebasoga@gmail.com'], email.to
+    assert_equal 'RubyGems Adoption Center - Adoption Request Rejected', email.subject
+  end
 end
