d9f4d35923a8684b6769c53fec0871d898ca04be
angela4713 <ang3l_gu@hotmail.com>
2015-09-22 23:32:46 -0500

    Fix indentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,12 @@ Rails.application.configure do
 
   #Mandrill configuration
   ActionMailer::Base.smtp_settings = {
-  address: "smtp.mandrillapp.com",
-  port: 587,
-  enable_starttls_auto: true,
-  user_name: ENV['MANDRILL_USERNAME'],
-  password: ENV['MANDRILL_PASSWORD'],
-  authentication: "login"
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_PASSWORD'],
+    authentication: "login"
   }
 
   ActionMailer::Base.delivery_method = :smtp
